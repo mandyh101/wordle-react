39272ea024833f597404454a54e986431b2bcee3
mandy <mandyhale10@gmail.com>
2023-02-12 18:46:02 +1300

    comment out react code for testing without internet

--- client/src/App.js
+++ client/src/App.js
@@ -3,23 +3,25 @@ import { useState, useEffect } from 'react'
 import Wordle from './components/Wordle'
 
 function App() {
-  const [guessedWord, setGuessedWord] = useState('SUPER')
-  const [wordle, setWordle] = useState('')
+  // const [guessedWord, setGuessedWord] = useState('SUPER')
+  // const [wordle, setWordle] = useState('')
 
   //TODO set up names route so people cant visit this URL to see the word
 
-  useEffect(() => {
-    const getWordle = () => {
-      fetch('http://localhost:8000/word')
-        .then((response) => response.json())
-        .then((json) => {
-          // console.log(json)
-          setWordle(json.toUpperCase())
-        })
-        .catch((err) => console.log(err))
-    }
-    getWordle()
-  }, [])
+  // useEffect(() => {
+  //   const getWordle = () => {
+  //     fetch('http://localhost:8000/word')
+  //       .then((response) => response.json())
+  //       .then((json) => {
+  //         // console.log(json)
+  //         setWordle(json.toUpperCase())
+  //       })
+  //       .catch((err) => console.log(err))
+  //   }
+  //   getWordle()
+  // }, [])
+
+  const wordle = 'HELLO'
 
   return (
     // <div className="App">

180dfeeb43cff4f7e123402cc70c1f860d70d545
mandy <mandyhale10@gmail.com>
2023-12-26 08:39:25 +1300

    adding some class names for styling

--- src/components/GameRow.js
+++ src/components/GameRow.js
@@ -3,11 +3,11 @@ import React from 'react'
 export default function GameRow({ guess, currentGuess }) {
   return (
     <div className="row">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div className="tile"></div>
+      <div className="tile"></div>
+      <div className="tile"></div>
+      <div className="tile"></div>
+      <div className="tile"></div>
     </div>
   )
 }
